b983816881f9dc0e2377d168d8270bce80406b7e
AnyuNivas <Srini@VENKATAs-MBP.midcoip.net>
2018-08-02 15:55:35 -0500

    js file changed

--- test.js
+++ test.js
@@ -1,15 +1,15 @@
 
 async function start(){
-	   img = document.getElementById('image').files[0];
+	img = document.getElementById('image').files[0];
 	
-        console.log(img)
+       
         
         model = await tf.loadModel('model/model.json')
         
         var status = document.getElementById('status')
-        console.log(status)
+      
         status.innerHTML = 'Model Loaded'
-        console.log(status)
+        
         //document.getElementById('status').innerHTML = 'Model Loaded';
 
         
@@ -17,9 +17,9 @@ async function start(){
     
         //load the class names
         await loadDict()
-        console.log('img')
+        }
    
 
-				}	
+					
 
 					  
