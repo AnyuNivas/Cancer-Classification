de6479fb15c31ca2aba99db6757a7c81c9787ba2
AnyuNivas <Srini@VENKATAs-MBP.midcoip.net>
2018-08-02 16:03:05 -0500

    js file changed

--- test.js
+++ test.js
@@ -1,4 +1,27 @@
 
+/*
+get the prediction 
+*/
+function predict(imgData) {
+        tf.tidy(() => {
+      
+                //get the prediction 
+                const pred = model.predict(preprocess(imgData)).dataSync()
+                    
+                //retreive the highest probability class label 
+                const idx = pred.argMax();
+                
+                //find the predictions 
+        const indices = findIndicesOfMax(pred, 1)
+        const probs = findTopValues(pred, 1)
+        const names = getClassNames(indices) 
+        //set the table 
+        //setTable(names, probs) 
+        document.getElementById("Result").innerHTML = names
+        document.getElementById("Probability").innerHTML = probs
+    });
+  }
+  
 async function start(){
 	img = document.getElementById('image').files[0];
 	
