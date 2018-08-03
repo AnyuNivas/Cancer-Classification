1415bea7071d991d5f98ab0a0a48b865826caeec
AnyuNivas <Srini@VENKATAs-MBP.midcoip.net>
2018-08-03 11:48:14 -0500

    js file changed

--- test.js
+++ test.js
@@ -70,10 +70,10 @@ function preprocess(img)
     const offset = tf.scalar(255.0);
     const normalized = tf.scalar(1.0).sub(resized.div(offset));
     //const resized = tf.image.resizeBilinear(normalized, [50, 50])
-    const sliced   = resized.slice([0,0,3], [50, 50, 3])
-    const batched = sliced.expandDims(0)
+    //const sliced   = resized.slice([0,0,3], [50, 50, 3])
+    //const batched = sliced.expandDims(0)
     //We add a dimension to get a batch shape 
-    //const batched = normalized.expandDims(0)
+    const batched = normalized.expandDims(0)
     return batched
 
 }
