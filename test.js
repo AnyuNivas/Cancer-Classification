edb9708cfecd1ed0327fa96827a02d09f2381748
AnyuNivas <Srini@VENKATAs-MBP.midcoip.net>
2018-08-06 14:06:51 -0500

    js file changed

--- test.js
+++ test.js
@@ -90,7 +90,7 @@ function predict(imgData) {
         //find the predictions 
         const indices = findIndicesOfMax(pred, 1)
         const probs = findTopValues(pred, 1)
-        const names = class_names(idx) 
+        const names = class_names[idx]
 
         //set the table 
         //setTable(names, probs) 
